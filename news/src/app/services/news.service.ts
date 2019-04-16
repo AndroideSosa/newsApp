ffa9a64fd11d5b18542673d33ebd70c90f85756f
Andres Chavez Sosa <andres.chavezsosa1494@gmail.com>
2019-04-16 18:05:10 -0500

    first commit

---
+++ news/src/app/services/news.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NewsService {
+
+  constructor( private http: HttpClient) { }
+
+  getTopHeadLines(){
+    return this.http.get('https://newsapi.org/v2/everything?q=bitcoin&from=2019-03-16&sortBy=publishedAt&apiKey=2fb898c315b04ad5b47806f1d021a304');
+  }
+}
---
+++ news/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiKey: '2fb898c315b04ad5b47806f1d021a304'
+};
